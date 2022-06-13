b11706ff4b71d4d956b0b5950d58936548f329e3
luo <1127959736@qq.com>
2022-06-13 15:02:16 +0800

    新增了PROVIDER_URL_SET，作为服务提供者的URL集合

--- src/main/java/irpc/framework/core/common/cache/CommonServerCache.java
+++ src/main/java/irpc/framework/core/common/cache/CommonServerCache.java
@@ -1,10 +1,17 @@
 package irpc.framework.core.common.cache;
 
+import irpc.framework.core.registry.URL;
+
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CommonServerCache {
 
     // 提供的服务集合，key为服务名，value为对应的服务
     public static final Map<String,Object> PROVIDER_CLASS_MAP = new HashMap<>();
+
+    //服务提供者的URL集合
+    public static final Set<URL> PROVIDER_URL_SET = new HashSet<>();
 }
