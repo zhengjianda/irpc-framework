850ddcdb5456181c2ae0ed86db883f03da42dd63
luo <1127959736@qq.com>
2022-06-13 15:16:41 +0800

    new URLChangeWrapper，包装器

---
+++ src/main/java/irpc/framework/core/common/event/data/URLChangeWrapper.java
@@ -0,0 +1,35 @@
+package irpc.framework.core.common.event.data;
+
+import java.util.List;
+
+public class URLChangeWrapper {
+
+    private String serviceName;
+
+    private List<String> providerUrl;
+
+    public String getServiceName(){
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public List<String> getProviderUrl() {
+        return providerUrl;
+    }
+
+    public void setProviderUrl(List<String> providerUrl) {
+        this.providerUrl = providerUrl;
+    }
+
+
+    @Override
+    public String toString() {
+        return "URLChangeWrapper{" +
+                "serviceName='" + serviceName + '\'' +
+                ", providerUrl=" + providerUrl +
+                '}';
+    }
+}
