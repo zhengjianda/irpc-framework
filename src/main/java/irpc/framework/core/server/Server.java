8fba7036079b15189bd7303e87245180b18751d9
luo <1127959736@qq.com>
2022-06-13 15:06:41 +0800

    主要是新增了batchExportUrl函数-将服务端的具体服务都暴露到注册中心，方便客户端进行调用

--- src/main/java/irpc/framework/core/server/Server.java
+++ src/main/java/irpc/framework/core/server/Server.java
@@ -11,18 +11,28 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import irpc.framework.core.common.RpcDecoder;
 import irpc.framework.core.common.RpcEncoder;
+import irpc.framework.core.common.config.PropertiesBootstrap;
 import irpc.framework.core.common.config.ServerConfig;
+import irpc.framework.core.common.utils.CommonUtils;
+import irpc.framework.core.registry.RegistryService;
+import irpc.framework.core.registry.URL;
+import irpc.framework.core.registry.zookeeper.ZookeeperRegister;
+import javafx.scene.layout.Region;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.net.Socket;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static irpc.framework.core.common.cache.CommonServerCache.PROVIDER_CLASS_MAP;
+import static irpc.framework.core.common.cache.CommonServerCache.PROVIDER_URL_SET;
 
 public class Server {
     private static EventLoopGroup bossGroup = null;
     private static EventLoopGroup workerGroup = null;
 
     private ServerConfig serverConfig;  //服务端配置
+
+    private RegistryService registryService;
 
     public ServerConfig getServerConfig(){
         return serverConfig;
@@ -31,6 +41,7 @@ public class Server {
     public static EventLoopGroup getBossGroup() {
         return bossGroup;
     }
+
 
     public static EventLoopGroup getWorkerGroup() {
         return workerGroup;
@@ -72,8 +83,66 @@ public class Server {
                 ch.pipeline().addLast(new ServerHandler()); //服务器处理器
             }
         });
-        bootstrap.bind(serverConfig.getPort()).sync();  //绑定端口
+        this.batchExportUrl();
+        bootstrap.bind(serverConfig.getServerPort()).sync();  //绑定端口
     }
+
+    public void initServerConfig(){
+        ServerConfig serverConfig = PropertiesBootstrap.loadServerConfigFromLocal();
+        this.setServerConfig(serverConfig);
+    }
+
+
+    /**
+     * 暴露服务信息
+     *
+     * @param serviceBean
+     */
+    public void exportService(Object serviceBean){
+        if (serviceBean.getClass().getInterfaces().length==0){
+            throw new RuntimeException("service must had interfaces!");
+        }
+        Class[] classes = serviceBean.getClass().getInterfaces();
+        if (classes.length>1){
+            throw new RuntimeException("service must only had one interfaces!");
+        }
+        if (registryService==null){
+            registryService = new ZookeeperRegister(serverConfig.getRegisterAddr());
+        }
+
+        //默认选择该对象的第一个实现接口
+        Class interfaceClass = classes[0];
+        PROVIDER_CLASS_MAP.put(interfaceClass.getName(), serviceBean);
+        URL url = new URL();
+        url.setServiceName(interfaceClass.getName());
+        url.setApplicationName(serverConfig.getApplicationName());
+        url.addParameter("host", CommonUtils.getIpAddress());
+        url.addParameter("port", String.valueOf(serverConfig.getServerPort()));
+        PROVIDER_URL_SET.add(url);
+    }
+
+    /**
+     * 这个函数的内部设计就是为了将服务端的具体服务都暴露到注册中心，方便客户端进行调用
+     */
+    public void batchExportUrl(){
+        Thread task = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (URL url:PROVIDER_URL_SET){
+                    registryService.register(url);
+                }
+            }
+        });
+        task.start();
+    }
+
+
+
 
     /**
      * 注册服务端的服务，相当于是注册了服务端中存在的本地方法，供给客户端远程调用
@@ -96,10 +165,8 @@ public class Server {
 
     public static void main(String[] args) throws InterruptedException {
         Server server = new Server();
-        ServerConfig serverConfig = new ServerConfig();
-        serverConfig.setPort(9090);
-        server.setServerConfig(serverConfig);
-        server.registryService(new DataServiceImpl());
+        server.initServerConfig();
+        server.exportService(new DataServiceImpl());
         server.startApplication();
     }
 }
