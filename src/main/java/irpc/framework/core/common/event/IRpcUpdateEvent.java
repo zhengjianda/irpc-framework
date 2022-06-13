0cab0346c8094b0e144289becfba3acabd6260d4
luo <1127959736@qq.com>
2022-06-13 15:12:26 +0800

    具体的事件，继承自IRpcEvent，为更新事件

---
+++ src/main/java/irpc/framework/core/common/event/IRpcUpdateEvent.java
@@ -0,0 +1,22 @@
+package irpc.framework.core.common.event;
+
+public class IRpcUpdateEvent implements IRpcEvent{
+
+    private Object data;
+
+
+    public IRpcUpdateEvent(Object data){
+        this.data = data;
+    }
+
+    @Override
+    public Object getData() {
+        return data;
+    }
+
+    @Override
+    public IRpcEvent setData(Object data) {
+        this.data = data;
+        return this;
+    }
+}
